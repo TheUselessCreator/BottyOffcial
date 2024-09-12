54f448d783d3abea2433f832b0fb5fe96c920401
TheUselessCreator <bot40games2@gmail.com>
2024-09-12 17:17:15 -0400

    Update setprefix.py
    Signed-off-by: TheUselessCreator <bot40games2@gmail.com>

--- Botty/PrefixCommands/setprefix.py
+++ Botty/PrefixCommands/setprefix.py
@@ -3,55 +3,39 @@ from discord.ext import commands
 import json
 import os
 
-# Path to the JSON file where prefixes are stored for each guild
-PREFIX_FILE_PATH = os.path.join(os.path.dirname(__file__), '../assets/prefixes.json')
-
-# Function to load all prefixes from the JSON file
-def load_prefixes():
-    try:
-        with open(PREFIX_FILE_PATH, 'r') as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return {}
-
-# Function to save prefixes to the JSON file
-def save_prefixes(prefixes):
-    with open(PREFIX_FILE_PATH, 'w') as f:
-        json.dump(prefixes, f, indent=4)
-
-# Function to get the prefix for a specific guild
-def get_guild_prefix(guild_id):
-    prefixes = load_prefixes()
-    return prefixes.get(str(guild_id), '/')  # Default prefix is '/'
-
-# Cog for setting and getting the prefix
-class SetPrefix(commands.Cog):
+class SetPrefixCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.prefixes = {}  # Dictionary to store prefixes
+        self.load_prefixes()  # Load prefixes from file on initialization
 
-    # Slash command to set the new prefix for the current guild
-    @commands.hybrid_command(name="setprefix")
-    async def setprefix(self, ctx, prefix: str):
-        """Sets a new prefix for the bot in the current server."""
-        if ctx.author.guild_permissions.administrator:
-            # Load existing prefixes
-            prefixes = load_prefixes()
-            # Set the new prefix for the current guild
-            prefixes[str(ctx.guild.id)] = prefix
-            save_prefixes(prefixes)
-            # Update the bot's prefix function
-            self.bot.command_prefix = self.get_prefix
-            await ctx.send(f"Prefix updated to: `{prefix}` for this server.")
+    # Load prefixes from JSON file
+    def load_prefixes(self):
+        if os.path.exists('prefixes.json'):
+            with open('prefixes.json', 'r') as f:
+                self.prefixes = json.load(f)
         else:
-            await ctx.send("You do not have permission to use this command.")
+            self.prefixes = {}
 
-    # Function to return the current prefix for each guild
+    # Save prefixes to JSON file
+    def save_prefixes(self):
+        with open('prefixes.json', 'w') as f:
+            json.dump(self.prefixes, f, indent=4)
+
+    # Command to set the prefix for a server
+    @commands.command(name="setprefix")
+    async def set_prefix(self, ctx, prefix):
+        """Sets a custom prefix for this server."""
+        guild_id = str(ctx.guild.id)
+        self.prefixes[guild_id] = prefix  # Set the new prefix for this server
+        self.save_prefixes()  # Save the updated prefixes to file
+        await ctx.send(f"Prefix set to `{prefix}` for this server.")
+
+    # Method to get the prefix for a server
     async def get_prefix(self, bot, message):
-        if message.guild:
-            return get_guild_prefix(message.guild.id)
-        else:
-            return '/'  # Default prefix in DMs
+        guild_id = str(message.guild.id)
+        return self.prefixes.get(guild_id, "/")  # Return the custom prefix or default "/" if not set
 
+# Setup function to load the cog
 async def setup(bot):
-    await bot.add_cog(SetPrefix(bot))
-
+    await bot.add_cog(SetPrefixCommand(bot))
