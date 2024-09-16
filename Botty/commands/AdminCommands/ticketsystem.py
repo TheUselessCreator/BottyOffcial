22161d7a3a5ae632a48c8a7eae6ad2012a6e2156
TheUselessCreator <bot40games2@gmail.com>
2024-09-16 04:24:05 -0400

    Update ticketsystem.py
    Signed-off-by: TheUselessCreator <bot40games2@gmail.com>

--- Botty/commands/AdminCommands/ticketsystem.py
+++ Botty/commands/AdminCommands/ticketsystem.py
@@ -64,43 +64,6 @@ class TicketSystem(commands.Cog):
         view.add_item(TicketDropdown())
         await channel.send("Please select your issue:", view=view)
 
-    @commands.Cog.listener()
-    async def on_interaction(self, interaction: discord.Interaction):
-        if interaction.type == discord.InteractionType.component:
-            if interaction.data['custom_id'] == 'claim_ticket':
-                if not any(role.id == self.staff_role_id for role in interaction.user.roles):
-                    await interaction.response.send_message("You do not have permission to claim tickets.", ephemeral=True)
-                    return
-                await interaction.response.send_message("Ticket claimed!", ephemeral=True)
-                # Handle claim logic here
-
-            elif interaction.data['custom_id'] == 'close_ticket':
-                if not any(role.id == self.staff_role_id for role in interaction.user.roles):
-                    await interaction.response.send_message("You do not have permission to close tickets.", ephemeral=True)
-                    return
-                await interaction.response.send_message("Please provide a reason for closing the ticket.", ephemeral=True)
-
-                class ReasonModal(Modal):
-                    def __init__(self):
-                        super().__init__(title="Close Ticket Reason")
-                        self.add_item(TextInput(label="Reason", placeholder="Enter the reason here..."))
-
-                    async def callback(self, interaction: discord.Interaction):
-                        reason = self.children[0].value
-                        await interaction.response.send_message(f"Ticket closed with reason: {reason}", ephemeral=True)
-                        await interaction.user.send(f"Your ticket has been closed with the following reason: {reason}")
-                        await interaction.channel.delete()
-
-                await interaction.response.send_modal(ReasonModal())
-
-            elif interaction.data['custom_id'] == 'close_ticket_no_reason':
-                if not any(role.id == self.staff_role_id for role in interaction.user.roles):
-                    await interaction.response.send_message("You do not have permission to close tickets.", ephemeral=True)
-                    return
-                await interaction.response.send_message("Your ticket has been closed!", ephemeral=True)
-                await interaction.user.send("Your ticket has been closed!")
-                await interaction.channel.delete()
-
 class TicketActions(View):
     def __init__(self):
         super().__init__()
